92f8733667be137fb10aef666ea6f70b0baa068b
XinoAssassin Ni <phnjs211@outlook.com>
2017-10-06 12:16:14 +0800

    add input

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import binascii
-
-for i in range(1,100000000):
-    if binascii.crc32(str(i).encode("utf-8")) == 0xde3bfc47:
+crc32 = input("Please input user CRC32:")
+hex_str = "0x" + crc32
+hex_int = int(hex_str,16)
+for i in range(1, 100000000):
+    if binascii.crc32(str(i).encode("utf-8")) == hex_int:
         print(i)
